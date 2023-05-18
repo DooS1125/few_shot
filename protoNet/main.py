d807439391cbb4a8124f193ac0f6413702cdf6d3
DooS1125 <ds020902@gmail.com>
2023-05-18 18:01:19 +0900

    protoNet code

--- protoNet/main.py
+++ protoNet/main.py
@@ -1,26 +1,20 @@
-import argparse
 import os.path as osp
 from pprint import pprint
 
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from tqdm import tqdm 
 
 from Data import *
 from Sampler import *
 from Proto import *
 from utils import *
-from tqdm import tqdm 
+from train import *
 
 import wandb
 
-save_path = './save'
-save_epoch = 20
-gpu = '0'
-test_batch = 100
-load_path = './save/max-acc.pth'
-
-hyperparameter_defaults = dict(max_epoch = 100, query = 5, 
+hyperparameter_defaults = dict(max_epoch = 100, query = 5, test_batch = 100,
                                shot = 5, way = 5, features = 'mel', win_length = 360, n_mels = 20, n_mfcc = 14, sr = 4000)
 
 wandb.init(config=hyperparameter_defaults, project="23_1_seminar")
@@ -33,176 +27,56 @@ else:
 wandb.run.name=run_name
 
 if __name__ == '__main__':
-
-    set_gpu(gpu)
-    ensure_path(save_path)
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     if config.features == 'mel':
         trainset = ESC_data('train', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        train_sampler = CategoriesSampler(trainset.label, 100,
-                                        config.way, config.shot + config.query)
-        train_loader = DataLoader(dataset=trainset, batch_sampler=train_sampler,
-                                  num_workers=0, pin_memory=True)
+        train_sampler = CategoriesSampler(trainset.label, 100, config.way, config.shot + config.query)
+        train_loader = DataLoader(dataset=trainset, batch_sampler=train_sampler, num_workers=0, pin_memory=True)
 
         valset = ESC_data('val', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        val_sampler = CategoriesSampler(valset.label, 50,
-                                        config.way, config.shot + config.query)
-        val_loader = DataLoader(dataset=valset, batch_sampler=val_sampler,
-                                num_workers=0, pin_memory=True)
+        val_sampler = CategoriesSampler(valset.label, 50, config.way, config.shot + config.query)
+        val_loader = DataLoader(dataset=valset, batch_sampler=val_sampler, num_workers=0, pin_memory=True)
     else:
         trainset = ESC_data('train', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        train_sampler = CategoriesSampler(trainset.label, 100,
-                                        config.way, config.shot + config.query)
-        train_loader = DataLoader(dataset=trainset, batch_sampler=train_sampler,
-                                  num_workers=0, pin_memory=True)
+        train_sampler = CategoriesSampler(trainset.label, 100, config.way, config.shot + config.query)
+        train_loader = DataLoader(dataset=trainset, batch_sampler=train_sampler, num_workers=0, pin_memory=True)
 
         valset = ESC_data('val', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        val_sampler = CategoriesSampler(valset.label, 50,
-                                        config.way, config.shot + config.query)
-        val_loader = DataLoader(dataset=valset, batch_sampler=val_sampler,
-                                num_workers=0, pin_memory=True)
+        val_sampler = CategoriesSampler(valset.label, 50, config.way, config.shot + config.query)
+        val_loader = DataLoader(dataset=valset, batch_sampler=val_sampler, num_workers=0, pin_memory=True)
 
 
-    model = Convnet().cuda()
+    model = Convnet().to(device)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
-
-    def save_model(name):
-        torch.save(model.state_dict(), osp.join(save_path, name + '.pth'))
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, 
+                                                              threshold_mode='abs', min_lr=1e-5, verbose=True)
     
-    train_loss=[]
-    val_loss=[]
-    train_acc=[]
-    val_acc=[]
-    max_acc=0.0
-
-    timer = Timer()
-
-    
-    for epoch in tqdm(range(1, config.max_epoch + 1), leave=True):
-
-        model.train()
-        
-        tl = Averager()
-        ta = Averager()
-        
-        for i, batch in tqdm(enumerate(train_loader, 1), leave=True):
-            data, _ = [_.cuda() for _ in batch]
-            p = config.shot * config.way
-            data_shot, data_query = data[:p], data[p:]
-
-            proto = model(data_shot)
-            proto = proto.reshape(config.shot, config.way, -1).mean(dim=0)
-
-            label = torch.arange(config.way).repeat(config.query)
-            label = label.type(torch.cuda.LongTensor)
-
-            logits = euclidean_metric(model(data_query), proto)
-            loss = F.cross_entropy(logits, label)
-            acc = count_acc(logits, label)
-            # print('epoch {}, train {}/{}, loss={:.4f} acc={:.4f}'
-            #       .format(epoch, i, len(train_loader), loss.item(), acc))
-
-            tl.add(loss.item())
-            ta.add(acc)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            proto = None; logits = None; loss = None
-            
-        lr_scheduler.step()
-
-        tl = tl.item()
-        ta = ta.item()
-
-        model.eval()
-
-        vl = Averager()
-        va = Averager()
-
-        for i, batch in enumerate(val_loader, 1):
-            data, _ = [_.cuda() for _ in batch]
-            p = config.shot * config.way
-            data_shot, data_query = data[:p], data[p:]
-
-            proto = model(data_shot)
-            proto = proto.reshape(config.shot, config.way, -1).mean(dim=0)
-
-            label = torch.arange(config.way).repeat(config.query)
-            label = label.type(torch.cuda.LongTensor)
-
-            logits = euclidean_metric(model(data_query), proto)
-            loss = F.cross_entropy(logits, label)
-            acc = count_acc(logits, label)
-
-            vl.add(loss.item())
-            va.add(acc)
-            
-            proto = None; logits = None; loss = None
-
-        vl = vl.item()
-        va = va.item()
-        print('epoch {}, val, loss={:.4f} acc={:.4f}'.format(epoch, vl, va))
-
-        if va > max_acc:
-            max_acc = va
-            save_model('max-acc')
-
-        train_loss.append(tl)
-        train_acc.append(ta)
-        val_loss.append(vl)
-        val_acc.append(va)
-        
-        save_model('epoch-last')
-
-        if epoch % save_epoch == 0:
-            save_model('epoch-{}'.format(epoch))
-
-        print('ETA:{}/{}'.format(timer.measure(), timer.measure(epoch / config.max_epoch)))
+    train_acc, train_loss, val_acc, val_loss = train(model, train_loader, val_loader, optimizer, lr_scheduler, device, config)
 
 
 if __name__ == '__main__':
-    set_gpu(gpu)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
     if config.features == 'mel':
         dataset = ESC_data('test', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        sampler = CategoriesSampler(dataset.label, test_batch, config.way, config.shot + config.query)
+        sampler = CategoriesSampler(dataset.label, config.test_batch, config.way, config.shot + config.query)
         loader = DataLoader(dataset, batch_sampler=sampler, num_workers=0, pin_memory=True)
     else:
         dataset = ESC_data('test', win_length = config.win_length, n_mels = config.n_mels, n_mfcc = config.n_mfcc, feature = config.features, sample_rate = config.sr)
-        sampler = CategoriesSampler(dataset.label, test_batch, config.way, config.shot + config.query)
+        sampler = CategoriesSampler(dataset.label, config.test_batch, config.way, config.shot + config.query)
         loader = DataLoader(dataset, batch_sampler=sampler, num_workers=0, pin_memory=True)
 
-    model = Convnet().cuda()
-    model.load_state_dict(torch.load(load_path))
-    model.eval()
-
-    ave_acc = Averager()
-
-    for i, batch in enumerate(loader, 1):
-        data, _ = [_.cuda() for _ in batch]
-        k = config.way * config.shot
-        data_shot, data_query = data[:k], data[k:]
-
-        x = model(data_shot)
-        x = x.reshape(config.shot, config.way, -1).mean(dim=0)
-        p = x
-
-        logits = euclidean_metric(model(data_query), p)
-
-        label = torch.arange(config.way).repeat(config.query)
-        label = label.type(torch.cuda.LongTensor)
-
-        acc = count_acc(logits, label)
-        ave_acc.add(acc)
-        print('batch {}: {:.2f}({:.2f})'.format(i, ave_acc.item() * 100, acc * 100))
-        
-        x = None; p = None; logits = None
+    model = Convnet().to(device)
+    model.load_state_dict(torch.load('./weights/best.pt'))
+    
+    test_loss, test_acc = inference(model, loader, device, config)
 
 
-wandb.run.summary['train_loss'] = tl
-wandb.run.summary['train_acc'] = ta
-wandb.run.summary['val_loss'] = vl
-wandb.run.summary['val_loss'] = va
-wandb.run.summary['ave_acc'] = acc
+wandb.run.summary['train_loss'] = train_loss[config.max_epoch-1]
+wandb.run.summary['train_acc'] = train_acc[config.max_epoch-1]
+wandb.run.summary['val_loss'] = val_loss[config.max_epoch-1]
+wandb.run.summary['val_acc'] = val_acc[config.max_epoch-1]
+wandb.run.summary['test_loss'] = test_loss
+wandb.run.summary['test_acc'] = test_acc
---
+++ protoNet/train.py
@@ -0,0 +1,122 @@
+import argparse
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from Data import *
+from Sampler import *
+from Proto import *
+from utils import *
+
+def validation(model, data_loader, device, config):
+    model.eval()
+    val_loss, val_acc = [], []
+    with torch.no_grad():
+        for batch in tqdm(data_loader):
+            data, _ = [_.to(device) for _ in batch]
+            p = config.shot * config.way
+            data_shot, data_query = data[:p], data[p:]
+
+            proto = model(data_shot)
+            proto = proto.reshape(config.shot, config.way, -1).mean(dim=0)
+
+            label = torch.arange(config.way).repeat(config.query).type(torch.cuda.LongTensor)
+
+            logits = euclidean_metric(model(data_query), proto)
+            loss = F.cross_entropy(logits, label)
+            acc = count_acc(logits, label)
+
+            val_loss.append(loss.item())
+            val_acc.append(acc)
+
+        return np.mean(val_loss), np.mean(val_acc)
+
+def train(model, train_loader, val_loader, optimizer, lr_scheduler, device, config):
+
+    early_stopping = EarlyStopping(patience=5, path='./weights/best.pt', verbose=True)
+    train_acc, train_loss, val_acc, val_loss = [], [], [], []
+
+    for epoch in range(1, config.max_epoch + 1):
+        model.train()
+        start_time = time.time()
+
+        _train_loss_epoch, _train_acc_epoch = [], []
+
+        for batch in tqdm(train_loader):
+            data, _ = [_.to(device) for _ in batch]
+            p = config.shot * config.way
+            data_shot, data_query = data[:p], data[p:]
+
+            proto = model(data_shot)
+            proto = proto.reshape(config.shot, config.way, -1).mean(dim=0)
+
+            label = torch.arange(config.way).repeat(config.query).type(torch.cuda.LongTensor)
+
+            logits = euclidean_metric(model(data_query), proto)
+            loss = F.cross_entropy(logits, label)
+            acc = count_acc(logits, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            _train_acc_epoch.append(acc)
+            _train_loss_epoch.append(loss.item())
+
+            train_acc_epoch = np.mean(_train_acc_epoch)
+            train_loss_epoch = np.mean(_train_loss_epoch)
+
+        train_acc.append(train_acc_epoch)
+        train_loss.append(train_loss_epoch)
+
+        val_loss_epoch, val_acc_epoch = validation(model, val_loader, device, config)
+
+        finish_time = time.time()
+        epoch_time = finish_time - start_time
+
+        print(f'Epoch [{epoch}], Train Loss : [{train_loss_epoch:.4f}] Train ACC : [{train_acc_epoch:.4f}] Val Loss : [{val_loss_epoch:.4f}] Val ACC : [{val_acc_epoch:.4f}] Epoch_time : [{epoch_time:.0f}s]')
+
+        val_loss.append(val_loss_epoch)
+        val_acc.append(val_acc_epoch)
+
+        if lr_scheduler is not None:
+            lr_scheduler.step(val_loss_epoch)
+
+        early_stopping(val_loss_epoch, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+    return train_acc, train_loss, val_acc, val_loss
+
+def inference(model, test_loader, device, config):
+    model.eval()
+    test_loss, test_acc = [], []
+    start_time = time.time()
+
+    with torch.no_grad():
+        for batch in tqdm(test_loader):
+            data, _ = [_.to(device) for _ in batch]
+            p = config.shot * config.way
+            data_shot, data_query = data[:p], data[p:]
+
+            proto = model(data_shot)
+            proto = proto.reshape(config.shot, config.way, -1).mean(dim=0)
+
+            label = torch.arange(config.way).repeat(config.query).type(torch.cuda.LongTensor)
+
+            logits = euclidean_metric(model(data_query), proto)
+            loss = F.cross_entropy(logits, label)
+            acc = count_acc(logits, label)
+
+            test_loss.append(loss.item())
+            test_acc.append(acc)
+
+        test_loss = np.mean(test_loss)
+        test_acc = np.mean(test_acc)
+
+        finish_time = time.time()
+        epoch_time = finish_time - start_time
+        print(f'Test Loss : [{test_loss:.4f}] Test ACC : [{test_acc:.4f}] Epoch_time : [{epoch_time:.0f}s]')
+
+    return test_loss, test_acc
